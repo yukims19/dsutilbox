df929f767d60bc9b11a709e6eaaba47cfb7c9502
yukims19 <yukims19@gmail.com>
2025-07-15 18:34:12 -0700

    Release v0.1.1

--- dsutilbox/__init__.py
+++ dsutilbox/__init__.py
@@ -2,7 +2,7 @@
 dsutilbox: Convenient utilities for data science workflows.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 # Optionally: expose some functions at top level
 # from .preprocessing import fill_missing_num_cols
